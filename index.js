272d0c120b90b66e23d731020a3e2a7f17889dd3
shuyang-he <she26@wustl.edu>
2020-06-23 10:40:54 -0700

    second

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
 (() => {
   "use strict";
 
-  const headerContainer = {
+  const topInfo = {
+    id: 'top-container'
+  }
+
+  const containerCreator = (topInfo, parent) => {
+    const container = document.createElement('div');
+    container.setAttribute('id', topInfo.id);
+    parent.append(container);
+  }
+
+  const headerInfo = {
     id: 'header-container',
     content: 'h1',
     text: 'w3schools'
   }
 
-  const headerContainerCreator = (headerContainer, parent) => {
+  const headerContainerCreator = (headerInfo, parent) => {
     const container = document.createElement('div');
-    container.setAttribute(headerContainer.id);
-    const content = document.createElement(headerContainer.content);
-    const text = document.createTextNode(headerContainer.text);
+    container.setAttribute('id', headerInfo.id);
+    const content = document.createElement(headerInfo.content);
+    const text = document.createTextNode(headerInfo.text);
     content.append(text);
     container.append(content);
     parent.append(container);
   }
 
-  const navigationContianer = {
-    id: 'navigation-contianer',
+  const navigationInfo = {
+    id: 'navigation-container',
     textList: ['HTML', 'CSS', 'JAVASCRIPT'],
     hrefList: ['/html', '/css', '/javascript']
   }
 
-  const navigationContainerCreator = (navigationContainer, parent) => {
+  const navigationContainerCreator = (navigationInfo, parent) => {
     const container = document.createElement('div');
-    container.setAttribute(navigationContainer.id);
-    for (let i = 0; i < navigationContainer.textList.length; i++) {
+    container.setAttribute('id', navigationInfo.id);
+    for (let i = 0; i < navigationInfo.textList.length; i++) {
       const link = document.createElement('a');
-      link.setAttribute('href', navigationContainer.hrefList[i]);
+      link.setAttribute('href', navigationInfo.hrefList[i]);
       const content = document.createElement('span');
-      const text = document.createTextNode(navigationContainer.textList[i]);
+      const text = document.createTextNode(navigationInfo.textList[i]);
       content.append(text);
       link.append(content);
       container.append(link);
@@ -38,262 +48,112 @@
     parent.append(container);
   }
 
-  const render = (element, container) => {
-    // when element type is string, create text node.
-    if (typeof element === 'string') {
-      const text = document.createTextNode(element);
-      container.append(element);
-      return;
+  containerCreator(topInfo, document.querySelector('#root-container'));
+  headerContainerCreator(headerInfo, document.querySelector('#top-container'));
+  navigationContainerCreator(navigationInfo, document.querySelector('#top-container'));
+  
+  const whiteboardInfo = {
+    id: 'whiteboard-container'
+  }
+
+  const sidebarInfo = {
+    id: 'sidebar-container'
+  }
+
+  const sidebarSectionInfo = {
+    class: 'main-section-container',
+    titleList: ['HTML Tutorial', 'HTML Forms'],
+    linkList: [
+      {
+        hrefList: ['/html_home', '/html_introduction'],
+        contentList: ['HTML Home', 'HTML Introduction']
+      },
+      {
+        hrefList: ['/html_forms', '/html_form_element'],
+        contentList: ['HTML Forms', 'HTML Form Element']
+      }
+    ]
+  }
+
+  const sidebarSectionCreator = (sidebarSectionInfo, parent) => {
+    for (let i = 0; i < sidebarSectionInfo.titleList.length; i++) {
+      const mainSection = document.createElement('div');
+      mainSection.setAttribute('class', sidebarSectionInfo.class);
+      const title = document.createElement('h2');
+      const titleContent = document.createTextNode(sidebarSectionInfo.titleList[i]);
+      title.append(titleContent);
+      mainSection.append(title);
+      for (let j = 0; j < sidebarSectionInfo.linkList[i].hrefList.length; j++) {
+        const link = document.createElement('a');
+        link.setAttribute('href', sidebarSectionInfo.linkList[i].hrefList[j]);
+        const span = document.createElement('span');
+        const spanContent = document.createTextNode(sidebarSectionInfo.linkList[i].contentList[j]);
+        span.append(spanContent);
+        link.append(span);
+        mainSection.append(link);
+      }
+      parent.append(mainSection);
     }
-
-    // create dom element tree recursively.
-    const newElement = document.createElement(element.type);
-    Object.keys(element.props).forEach((prop) => {
-      // children prop is representing children of element.
-      if (prop === 'children') {
-        let children = element.props[prop];
-        children = Array.isArray(children) ? children : [children];
-        children.forEach((child) => {
-          render(child, newElement);
-        });
-      }
-      newElement.setAttribute(prop, element.props[prop])
-    })
-
-    container.append(newElement);
   }
 
-  const createElement = (type, config, children) => {
-    const props = {};
-    Object.keys(config).forEach((prop) => {
-      if (config.hasOwnProperty(prop)) {
-        props[prop] = config[prop];
-      }
-    })
+  containerCreator(whiteboardInfo, document.querySelector('#root-container'));
+  containerCreator(sidebarInfo, document.querySelector('#whiteboard-container'));
+  sidebarSectionCreator(sidebarSectionInfo, document.querySelector('#sidebar-container'));
 
-    props.children = children
-
-    const element = {
-      type: type,
-      props: props
-    };
-    return JSON.parse(JSON.stringify(element));
+  const bodyInfo = {
+    id: 'body-container'
   }
 
-  // start build components
+  const contentInfo = {
+    id: 'content-container'
+  }
 
-  const headerContainerH1 = createElement(
-    'h1',
-    {},
-    'w3schools'
-  );
+  const mainInfo = {
+    id: 'main-container'
+  }
 
-  const headerContainer = createElement(
-    'div',
-    {id: 'header-container'},
-    headerContainerH1
-  );
+  const rightInfo = {
+    id: 'right-container'
+  }
 
-  const navigationContainerA1Span = createElement(
-    'span',
-    {},
-    'HTML'
-  );
+  containerCreator(bodyInfo, document.querySelector('#whiteboard-container'));
+  containerCreator(contentInfo, document.querySelector('#body-container'));
+  containerCreator(mainInfo, document.querySelector('#content-container'));
+  containerCreator(rightInfo, document.querySelector('#content-container'));
 
-  const navigationContainerA1 = createElement(
-    'a',
-    {href: '/html'},
-    navigationContainerA1Span
-  );
+  const footerInfo = {
+    id: 'footer-container'
+  }
 
-  const navigationContainerA2Span = createElement(
-    'span',
-    {},
-    'CSS'
-  );
+  containerCreator(footerInfo, document.querySelector('#body-container'));
 
-  const navigationContainerA2 = createElement(
-    'a',
-    {href: '/css'},
-    navigationContainerA2Span
-  );
+  const mainSectionInfo = {
+    class: 'main-section-container',
+    contentList: ['Main section 1', 'Main section 2']
+  }
 
-  const navigationContainerA3Span = createElement(
-    'span',
-    {},
-    'JAVASCRIPT'
-  );
+  const rightSectionInfo = {
+    class: 'right-section-container',
+    contentList: ['Right section 1', 'Right section 2']
+  }
 
-  const navigationContainerA3 = createElement(
-    'a',
-    {href: '/javascript'},
-    navigationContainerA3Span
-  );
+  const footerSectionInfo = {
+    class: 'footer-section-container',
+    contentList: ['Footer section 1', 'Footer section 2']
+  }
 
-  const navigationContainer = createElement(
-    'div',
-    {id: 'navigation-container'},
-    [navigationContainerA1, navigationContainerA2, navigationContainerA3]
-  );
+  const classCreator = (classInfo, parent) => {
+    for (let i = 0; i < classInfo.contentList.length; i++) {
+      const container = document.createElement('div');
+      container.setAttribute('class', classInfo.class);
+      const content = document.createTextNode(classInfo.contentList[i]);
+      container.append(content);
+      parent.append(container);
+    }
+  }
 
-  const topContainer = createElement(
-    'div',
-    {id: 'top-container'},
-    [headerContainer, navigationContainer]
-  );
-
-  render(topContainer, document.querySelector('#root-container'));
-
-  const menuSectionContainer1H2 = createElement(
-    'h2',
-    {},
-    'HTML Tutorial'
-  );
-
-  const menuSectionContainer1A1Span = createElement(
-    'span',
-    {},
-    'HTML Home'
-  );
-
-  const menuSectionContainer1A1 = createElement(
-    'a',
-    {href: '/html_home'},
-    menuSectionContainer1A1Span
-  );
-
-  const menuSectionContainer1A2Span = createElement(
-    'span',
-    {},
-    'HTML Introduction'
-  );
-
-  const menuSectionContainer1A2 = createElement(
-    'a',
-    {href: '/html_introduction'},
-    menuSectionContainer1A2Span
-  );
-
-  const menuSectionContainer1 = createElement(
-    'div',
-    {class: 'menu-section-container'},
-    [menuSectionContainer1H2, menuSectionContainer1A1, menuSectionContainer1A2]
-  );
-
-  const menuSectionContainer2H2 = createElement(
-    'h2',
-    {},
-    'HTML Tutorial'
-  );
-
-  const menuSectionContainer2A1Span = createElement(
-    'span',
-    {},
-    'HTML Home'
-  );
-
-  const menuSectionContainer2A1 = createElement(
-    'a',
-    {href: '/html_home'},
-    menuSectionContainer2A1Span
-  );
-
-  const menuSectionContainer2A2Span = createElement(
-    'span',
-    {},
-    'HTML Introduction'
-  );
-
-  const menuSectionContainer2A2 = createElement(
-    'a',
-    {href: '/html_introduction'},
-    menuSectionContainer2A2Span
-  );
-
-  const menuSectionContainer2 = createElement(
-    'div',
-    {class: 'menu-section-container'},
-    [menuSectionContainer2H2, menuSectionContainer2A1, menuSectionContainer2A2]
-  );
-
-  const sidebarContainer = createElement(
-    'div',
-    {id: 'sidebar-container'},
-    [menuSectionContainer1, menuSectionContainer2]
-  );
-
-  const mainSectionContainer1 = createElement(
-    'div',
-    {class: 'main-section-container'},
-    'Main Section 1'
-  );
-
-  const mainSectionContainer2 = createElement(
-    'div',
-    {class: 'main-section-container'},
-    'Main Section 2'
-  );
-
-  const mainContainer = createElement(
-    'div',
-    {id: 'main-container'},
-    [mainSectionContainer1, mainSectionContainer2]
-  );
-
-  const rightSectionContainer1 = createElement(
-    'div',
-    {class: 'right-section-container'},
-    'Right Section 1'
-  );
-
-  const rightSectionContainer2 = createElement(
-    'div',
-    {class: 'right-section-container'},
-    'Right Section 2'
-  );
-
-  const rightContainer = createElement(
-    'div',
-    {id: 'right-container'},
-    [rightSectionContainer1, rightSectionContainer2]
-  );
-
-  const contentContainer = createElement(
-    'div',
-    {id: 'content-container'},
-    [mainContainer, rightContainer]
-  );
-
-  const footerSectionContainer1 = createElement(
-    'div',
-    {class: 'footer-section-container'},
-    'Footer Section 1'
-  );
-
-  const footerSectionContainer2 = createElement(
-    'div',
-    {class: 'footer-section-container'},
-    'Footer Section 2'
-  );
-
-  const footerContainer = createElement(
-    'div',
-    {id: 'footer-container'},
-    [footerSectionContainer1, footerSectionContainer2]
-  );
-
-  const bodyContainer = createElement(
-    'div',
-    {id: 'body-container'},
-    [contentContainer, footerContainer]
-  );
-
-  const whiteboardContainer = createElement(
-    'div',
-    {id: 'whiteboard-container'},
-    [sidebarContainer, bodyContainer]
-  );
-
-  render(whiteboardContainer, document.querySelector('#root-container'));
+  classCreator(mainSectionInfo, document.querySelector('#main-container'));
+  classCreator(rightSectionInfo, document.querySelector('#right-container'));
+  classCreator(footerSectionInfo, document.querySelector('#footer-container'));
 
 })();
